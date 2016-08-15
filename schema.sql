e5257fe81fe03b41e2573301ada1f01728433928
Shay Kennedy <kennedy.rshay@gmail.com>
2016-08-15 15:34:41 +0000

    Initial commit

---
+++ schema.sql
@@ -0,0 +1,19 @@
+create table if not exists recipes (
+    id serial primary key,
+    name text not null,
+    description text not null
+);
+
+create table if not exists tags (
+    id serial primary key,
+    tag text not null
+);
+
+create table if not exists steps (
+    step text not null
+    recipe_id integer references recipes
+    tags_id integer reference tags
+    primary key(recipe_id, tags_id)
+);
+    
+
---
+++ schema2.sql
@@ -0,0 +1,22 @@
+create table if not exists recipes (
+    id serial primary key,
+    name text not null,
+    description text not null
+);
+
+create table if not exists tags (
+    id serial primary key,
+    tag text not null
+);
+
+create table if not exists recipes_tags(
+    recipe_id integer references recipes,
+    tags_id integer references tags,
+    primary key(recipe_id, tags_id)
+);
+
+create table if not exists steps (
+    step text not null,
+    recipe_id integer references recipes,
+    id serial primary key
+);
